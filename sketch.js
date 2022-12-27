b9ea3aa95fa5d0310c86ff97681321366ce885f0
Zaid42069 <109572827+Zaid42069@users.noreply.github.com>
2022-12-27 11:04:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+var bg,maze;
+var player,enemy1,enemy2,enemy3,enemy4;
+var playerIsRunning;
+keys = []
+ 
+function setup(){
+ maze = loadImage("./assets/bg around/maze.png")
+ player =loadImage("./assets/player/1og.png")
+}
